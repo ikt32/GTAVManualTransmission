3c4b246a7e5d729d908221c20b703139111a5d25
E66666666 <emily66666666ok@outlook.com>
2016-06-09 22:09:46 +0200

    Create DirectInput wheel class

--- Gears/WheelDirectInput.hpp
+++ Gears/WheelDirectInput.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+
+#include "..\..\ScriptHookV_SDK\inc\types.h"
+#include "ScriptSettings.hpp"
+
+enum class InputType {
+	Wheel,
+	Throttle,
+	Brake,
+	Clutch,
+	Buttons,
+	POVHat
+};
+
+class WheelDirectInput {
+public:
+	WheelDirectInput();
+	bool Init(ScriptSettings &settings);
+	bool Update();
+
+	void Steer(float steerVal);
+	void PlayEffects(
+		Vector3 &accelVals,
+		ScriptSettings &settings);
+	float GetValue(InputType type);
+private:
+
+
+};
