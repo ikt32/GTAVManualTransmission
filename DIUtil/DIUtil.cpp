97ef438e21689f9ccebdbe2a254d3ce04de9cbd8
E66666666 <emily66666666ok@outlook.com>
2016-11-30 20:01:59 +0100

    Remove unused files

--- DIUtil/DIUtil.cpp
+++ DIUtil/DIUtil.cpp
@@ -1,7 +1,7 @@
 // DIUtil.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 #include "../Gears/Input/WheelDirectInput.hpp"
 
