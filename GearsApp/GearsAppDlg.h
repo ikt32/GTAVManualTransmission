31b519b82eb75d6582c0fdfba5df24134376fd67
E66666666 <emily66666666ok@outlook.com>
2016-12-21 20:52:11 +0100

    Add settings application

---
+++ GearsApp/GearsAppDlg.h
@@ -0,0 +1,42 @@
+
+// GearsAppDlg.h : header file
+//
+
+#pragma once
+#include <string>
+#include "../Gears/ScriptSettings.hpp"
+#include "../Gears/Input/ScriptControls.hpp"
+
+
+// CGearsAppDlg dialog
+class CGearsAppDlg : public CDialogEx
+{
+// Construction
+public:
+	CGearsAppDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GEARSAPP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	ScriptControls controls;
+	ScriptSettings settings;
+	Logger logger;
+};
