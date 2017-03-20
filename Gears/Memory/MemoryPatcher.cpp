01ddb81873cb820ace0bfd87490b7863f17cd6cd
E66666666 <emily66666666ok@outlook.com>
2017-03-20 17:34:09 +0100

    Remove a bunch of commented out stuff

--- Gears/Memory/MemoryPatcher.cpp
+++ Gears/Memory/MemoryPatcher.cpp
@@ -251,24 +251,26 @@ namespace MemoryPatcher {
 	}
 
 	uintptr_t PatchSteering() {
-		// F3 44 0F 10 15 03 31 94 00
+		// GTA V (b791.2 to b1011.1)
+		// F3 44 0F10 15 03319400
 		// 45 84 ED				// test		r13l,r13l
-		// 0F 84 C8010000		// je		GTA5.exe+EXE53E // means jump if 0 in this case
-		// 0F 28 4B 70 (Next)	// movaps	xmm1,[rbx+70]
+		// 0F84 C8010000		// je		GTA5.exe+EXE53E
+		// 0F28 4B 70 (Next)	// movaps	xmm1,[rbx+70]
 		// F3 0F10 25 06F09300
 		// F3 0F10 1D 3ED39F00
 		
 		// in 791.2, 877.1 and 944.2
-		// 0F 84 D0 01 00 00 = JE	[some address]
+		// 0F 84 D0010000 = JE	<address>
 		// becomes
-		// E9 D1 01 00 00 90 = JMP	[some address]
+		// E9 D1 01000090 = JMP	<address> NOP
 
 		// InfamousSabre
 		// "\x0F\x84\x00\x00\x00\x00\x0F\x28\x4B\x70\xF3\x0F\x10\x25\x00\x00\x00\x00\xF3\x0F\x10\x1D\x00\x00\x00\x00" is what I scan for.
 
+		// FiveM (b505.2)
 		// F3 44 0F10 15 03319400
 		// 45 84 ED             
-		// 0F84 D0010000        
+		// 0F84 D0010000        // <- This one
 		// 0F28 4B 70           
 		// F3 0F10 25 92309400  
 		// F3 0F10 1D 6A309400  
@@ -280,14 +282,12 @@ namespace MemoryPatcher {
 		}
 		else {
 			address = mem.FindPattern(
-				//"\x45\x84\xED"
 				"\x0F\x84\xD0\x01\x00\x00" // <- This one
 				"\x0F\x28\x4B\x70"
 				"\xF3\x0F\x10\x25\x00\x00\x00\x00"
 				"\xF3\x0F\x10\x1D\x00\x00\x00\x00"
 				"\x0F\x28\xC1"
 				"\x0F\x28\xD1",
-				//"xx?"
 				"xx????" // <- This one
 				"xx??"
 				"xxxx????"
@@ -297,8 +297,6 @@ namespace MemoryPatcher {
 		}
 
 		if (address) {
-			/*ptrdiff_t offset = 0;
-			address = address + offset;*/
 			byte instrArr[6] = { 0xE9, 0xD1, 0x01, 0x00, 0x00, 0x90 };	// make preliminary instruction: JMP to <adrr>
 
 			memcpy(origSteerInstr, (void*)address, 6);					// save whole orig instruction
@@ -312,16 +310,8 @@ namespace MemoryPatcher {
 	}
 
 	void RestoreSteering(uintptr_t address, byte *origInstr, int origInstrSz) {
-		//byte instrArr[6] = { 0x0F, 0x84, 0xD0, 0x01, 0x00, 0x00 };
-		/*byte instrArr[6] = { 0x0F, 0x84, 0xD0, 0x01, 0x00, 0x00 };
-		for (int i = 0; i < 6; i++) {
-			memset(instrArr + i, origSteerInstr[i], 1);
-		}*/
 		if (address) {
 			memcpy((void*)address, origInstr, origInstrSz);
-			/*for (int i = 0; i < 6; i++) {
-				memset(reinterpret_cast<void *>(address + i), instrArr[i], 1);
-			}*/
 		}
 	}
 }
