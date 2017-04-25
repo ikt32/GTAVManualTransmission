ikt32/GTAVManualTransmission

commit 30998a349a7f773291eee8609c126914ee35011d

Add sound class
:/

Gears/Util/Util.cpp
@@ -19,14 +19,17 @@ void showText(float x, float y, float scale, const char* text, int font, const C
 }
 
 void showNotification(const char* message, int *prevNotification) {
-	if (prevNotification && *prevNotification != 0) {
+	if (prevNotification != nullptr && *prevNotification != 0) {
 		UI::_REMOVE_NOTIFICATION(*prevNotification);
 	}
 	UI::_SET_NOTIFICATION_TEXT_ENTRY("STRING");
 
 	UI::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(CharAdapter(message));
 	
-	*prevNotification = UI::_DRAW_NOTIFICATION(false, false);
+	int id = UI::_DRAW_NOTIFICATION(false, false);
+	if (prevNotification != nullptr) {
+		*prevNotification = id;
+	}
 }
 
 // gracefully borrowed from FiveM <3
@@ -42,3 +45,30 @@ void showSubtitle(std::string message, int duration) {
 
 	UI::END_TEXT_COMMAND_PRINT(duration, 1);
 }
+
+GameSound::GameSound(char *sound, char *soundSet) {
+	Active = false;
+	sound = sound;
+	soundSet = soundSet;
+	soundID = -1;
+}
+
+GameSound::~GameSound() {
+	AUDIO::RELEASE_SOUND_ID(soundID);
+}
+
+void GameSound::Load(char *audioBank) {
+	AUDIO::REQUEST_SCRIPT_AUDIO_BANK(audioBank, false);
+}
+
+void GameSound::Play(Entity ent) {
+	soundID = AUDIO::GET_SOUND_ID();
+	AUDIO::PLAY_SOUND_FROM_ENTITY(soundID, sound, ent, soundSet, 0, 0);
+}
+
+void GameSound::Stop() {
+	if (soundID == -1 || !Active) return;
+	AUDIO::STOP_SOUND(soundID);
+	Active = false;
+}
+

Gears/Util/Util.hpp
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <vector>
+#include "../../ScriptHookV_SDK/inc/types.h"
 
 static std::vector<std::string> GameVersionString = {
 	"VER_1_0_335_2_STEAM", // 00
@@ -149,4 +150,22 @@ public:
 
 private:
 	char* m_s;
+};
+
+//https://github.com/CamxxCore/AirSuperiority
+class GameSound {
+public:
+	GameSound(char *sound, char *soundSet);
+	~GameSound();
+	void Load(char *audioBank);
+	void Play(Entity ent);
+	void Stop();
+
+	bool Active;
+
+private:
+	char *soundSet;
+	char *sound;
+	int soundID;
+	int prevNotification;
 };

commit ff1e98536d4fb5e2672d4e03e87834ebe644b2e2

Fix exit game crash in GameSound destructor
0fd8cec and 30998a3 caused the game to crash on exit when the global
object GameSound was destroyed. This was a bitch to find, especially
because I had no idea what caused it at first. Should be good now.

Gears/Util/Util.cpp
@@ -54,7 +54,7 @@ GameSound::GameSound(char *sound, char *soundSet): m_prevNotification(0) {
 }
 
 GameSound::~GameSound() {
-	AUDIO::STOP_SOUND(m_soundID);
+	if (m_soundID == -1 || !Active) return;
 	AUDIO::RELEASE_SOUND_ID(m_soundID);
 }
 