6ac159e6a29cb291e01d5a712a9c8d049e0ef326
E66666666 <emily66666666ok@outlook.com>
2018-08-01 19:41:19 +0200

    Improve previous highest version finder search thing

--- Gears/main.cpp
+++ Gears/main.cpp
@@ -18,14 +18,11 @@ namespace fs = std::experimental::filesystem;
 
 template < typename K, typename V >
 V findNextLowest(const std::map<K, V>& map, K key) {
-    V lowestMatch{};
     for (auto it = map.rbegin(); it != map.rend(); ++it) {
-        if (it->first <= key) {
-            lowestMatch = it->second;
-            return lowestMatch;
-        }
+        if (it->first <= key)
+            return it->second;
     }
-    return lowestMatch;
+    return V{};
 }
 
 int getExeVersion(std::string newestExe) {
