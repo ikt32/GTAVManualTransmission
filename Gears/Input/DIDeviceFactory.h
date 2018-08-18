bbce14b3f1ec350235880c0adb5d922f53868729
E66666666 <emily66666666ok@outlook.com>
2018-08-19 01:13:18 +0200

    Change to Windows XInput/dinput8/dxguid libs

--- Gears/Input/DIDeviceFactory.h
+++ Gears/Input/DIDeviceFactory.h
@@ -6,8 +6,6 @@
 #pragma once
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
 
 struct DIDevice {
     DIDEVICEINSTANCE diDeviceInstance;
