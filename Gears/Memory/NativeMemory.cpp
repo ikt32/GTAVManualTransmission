7edaf24ffc4a1ee0f59ddc5b9ef650434bc638cb
E66666666 <emily66666666ok@outlook.com>
2019-01-02 15:44:20 +0100

    Add FindPattern variant with multiple things

--- Gears/Memory/NativeMemory.cpp
+++ Gears/Memory/NativeMemory.cpp
@@ -49,4 +49,29 @@ namespace mem {
         }
         return 0;
     }
+
+    std::vector<uintptr_t> FindPatterns(const char* pattern, const char* mask) {
+        std::vector <uintptr_t> addresses;
+
+        MODULEINFO modInfo = { nullptr };
+        GetModuleInformation(GetCurrentProcess(), GetModuleHandle(nullptr), &modInfo, sizeof(MODULEINFO));
+
+        const char* start_offset = reinterpret_cast<const char *>(modInfo.lpBaseOfDll);
+        const uintptr_t size = static_cast<uintptr_t>(modInfo.SizeOfImage);
+
+        intptr_t pos = 0;
+        const uintptr_t searchLen = static_cast<uintptr_t>(strlen(mask) - 1);
+
+        for (const char* retAddress = start_offset; retAddress < start_offset + size; retAddress++) {
+            if (*retAddress == pattern[pos] || mask[pos] == '?') {
+                if (mask[pos + 1] == '\0')
+                    addresses.push_back(reinterpret_cast<uintptr_t>(retAddress) - searchLen);
+                pos++;
+            }
+            else {
+                pos = 0;
+            }
+        }
+        return addresses;
+    }
 }
--- Gears/Memory/NativeMemory.hpp
+++ Gears/Memory/NativeMemory.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
+#include <vector>
 
 namespace mem {
-    void init();
-    uintptr_t FindPattern(const char* pattern, const char* mask); 
-    extern uintptr_t(*GetAddressOfEntity)(int entity);
-    extern uintptr_t(*GetModelInfo)(unsigned int modelHash, int* index);
+void init();
+uintptr_t FindPattern(const char* pattern, const char* mask); 
+std::vector<uintptr_t> FindPatterns(const char* pattern, const char* mask);
+extern uintptr_t(*GetAddressOfEntity)(int entity);
+extern uintptr_t(*GetModelInfo)(unsigned int modelHash, int* index);
 }
