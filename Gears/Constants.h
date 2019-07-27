faa186b9017e43cbbe1b7723ce3d71ed578b50a3
E66666666 <emily66666666ok@outlook.com>
2019-07-27 22:44:05 +0200

    Bump version number

--- Gears/Constants.h
+++ Gears/Constants.h
@@ -4,7 +4,7 @@
 
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 6
-#define VERSION_PATCH 7
+#define VERSION_PATCH 8
 
 static const char* const DISPLAY_VERSION = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH);
 static const char* const mtDir = "\\ManualTransmission";
