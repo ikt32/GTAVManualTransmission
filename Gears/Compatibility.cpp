2e9cac368a58546a5f5380be25081ff986782af9
E66666666 <emily66666666ok@outlook.com>
2019-08-12 18:53:16 +0200

    Set compat ptrs to nullptr

--- Gears/Compatibility.cpp
+++ Gears/Compatibility.cpp
@@ -35,6 +35,7 @@ void setupCompatibility() {
 
 void releaseCompatibility() {
     if (TrainerVModule && TrainerV::Active) {
-        //FreeLibrary(TrainerVModule);
+        TrainerVModule = nullptr;
+        TrainerV::Active = nullptr;
     }
 }
