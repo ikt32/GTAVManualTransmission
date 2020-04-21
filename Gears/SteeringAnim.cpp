82ceb8fd68c7ab554791871d70282364fceb7c9a
E66666666 <emily66666666ok@outlook.com>
2020-04-22 01:02:15 +0200

    Refactor animation playing

--- Gears/SteeringAnim.cpp
+++ Gears/SteeringAnim.cpp
@@ -28,6 +28,7 @@ int g_steerAnimDictIdx = 0;
 
 std::vector<std::string> g_steerAnimDicts{
     "veh@std@ds@base", // 720
+//    "veh@std@ds@idle_duck", // 720
     "veh@low@front_ds@base", // 360 :(
     "veh@truck@ds@base", // 720
     "veh@van@ds@base", // 720
@@ -41,15 +42,15 @@ std::vector<std::string> g_steerAnimDicts{
 //    {  }
 //};
 
-//enum eAnimationFlags
-//{
-//    ANIM_FLAG_NORMAL = 0,
-//    ANIM_FLAG_REPEAT = 1,
-//    ANIM_FLAG_STOP_LAST_FRAME = 2,
-//    ANIM_FLAG_UPPERBODY = 16,
-//    ANIM_FLAG_ENABLE_PLAYER_CONTROL = 32,
-//    ANIM_FLAG_CANCELABLE = 120,
-//};
+enum eAnimationFlags
+{
+    ANIM_FLAG_NORMAL = 0,
+    ANIM_FLAG_REPEAT = 1,
+    ANIM_FLAG_STOP_LAST_FRAME = 2,
+    ANIM_FLAG_UPPERBODY = 16,
+    ANIM_FLAG_ENABLE_PLAYER_CONTROL = 32,
+    ANIM_FLAG_CANCELABLE = 120,
+};
 
 
 // map
@@ -63,14 +64,10 @@ float figureOutAnimModulation(float wheelDegrees, float maxAnimAngle) {
     return animTime;
 }
 
+void playAnimTime(const std::string& dict, const std::string& anim, float time);
+
 void UpdateSteeringAnimations(float wheelDegrees) {
     const std::string& dict = g_steerAnimDicts[g_steerAnimDictIdx];
-    const bool playing = ENTITY::IS_ENTITY_PLAYING_ANIM(g_playerPed, dict.c_str(), "steer_no_lean", 3);
-    if (!g_customAnim) {
-        if (playing)
-            AI::STOP_ANIM_TASK(g_playerPed, dict.c_str(), "steer_no_lean", 8.0f);
-        return;
-    }
 
     float maxAnimAngle = 720.0f;
     if (dict == "veh@low@front_ds@base" ||
@@ -81,10 +78,15 @@ void UpdateSteeringAnimations(float wheelDegrees) {
     // div 2 cuz one-way angle
     float finalSteerVal = figureOutAnimModulation(wheelDegrees, maxAnimAngle / 2.0f);
 
-    showText(0.1f, 0.00f, 0.5f, fmt::format("maxAnimAngle: {}", maxAnimAngle));
-    showText(0.1f, 0.10f, 0.5f, fmt::format("wheelDegs: {}", wheelDegrees));
-    showText(0.1f, 0.15f, 0.5f, fmt::format("finalSteerVal: {}", finalSteerVal));
+    //showText(0.1f, 0.00f, 0.5f, fmt::format("maxAnimAngle: {}", maxAnimAngle));
+    //showText(0.1f, 0.10f, 0.5f, fmt::format("wheelDegs: {}", wheelDegrees));
+    //showText(0.1f, 0.15f, 0.5f, fmt::format("finalSteerVal: {}", finalSteerVal));
 
+    playAnimTime(dict, "steer_no_lean", finalSteerVal);
+}
+
+void playAnimTime(const std::string& dict, const std::string& anim, float time) {
+    const bool playing = ENTITY::IS_ENTITY_PLAYING_ANIM(g_playerPed, dict.c_str(), anim.c_str(), 3);
 
     if (!playing) {
         // TODO: Handle errors
@@ -98,10 +100,10 @@ void UpdateSteeringAnimations(float wheelDegrees) {
             }
             WAIT(0);
         }
-
-        AI::TASK_PLAY_ANIM(g_playerPed, dict.c_str(), "steer_no_lean", -8.0f, 8.0f, -1, 48, finalSteerVal, 0, 0, 0);
+        constexpr int flag = /*ANIM_FLAG_UPPERBODY | */ANIM_FLAG_ENABLE_PLAYER_CONTROL;
+        AI::TASK_PLAY_ANIM(g_playerPed, dict.c_str(), anim.c_str(), -8.0f, 8.0f, -1, flag, 1.0f, 0, 0, 0);
     }
     else {
-        ENTITY::SET_ENTITY_ANIM_CURRENT_TIME(g_playerPed, dict.c_str(), "steer_no_lean", finalSteerVal);
+        ENTITY::SET_ENTITY_ANIM_CURRENT_TIME(g_playerPed, dict.c_str(), anim.c_str(), time);
     }
 }
