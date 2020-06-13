53ed026d26f4dceb6669712e88b0a809cfb9976a
E66666666 <emily66666666ok@outlook.com>
2020-06-14 01:50:24 +0200

    Add "FF ? FF ? FF"-style pattern finder
    That actually isn't used lmaoooo

--- Gears/Memory/NativeMemory.cpp
+++ Gears/Memory/NativeMemory.cpp
@@ -1,8 +1,27 @@
 #include "NativeMemory.hpp"
 
+#include "../Util/Logger.hpp"
 #include <Windows.h>
 #include <Psapi.h>
-#include "../Util/Logger.hpp"
+#include <sstream>
+
+namespace {
+    template<typename Out>
+    void split(const std::string& s, char delim, Out result) {
+        std::stringstream ss;
+        ss.str(s);
+        std::string item;
+        while (std::getline(ss, item, delim)) {
+            *(result++) = item;
+        }
+    }
+
+    std::vector<std::string> split(const std::string& s, char delim) {
+        std::vector<std::string> elems;
+        ::split(s, delim, std::back_inserter(elems));
+        return elems;
+    }
+}
 
 namespace mem {
 
@@ -74,4 +93,30 @@ namespace mem {
         }
         return addresses;
     }
+
+uintptr_t FindPattern(const char* pattStr) {
+    std::vector<std::string> bytesStr = split(pattStr, ' ');
+
+    MODULEINFO modInfo{};
+    GetModuleInformation(GetCurrentProcess(), GetModuleHandle(nullptr), &modInfo, sizeof(MODULEINFO));
+
+    auto* start_offset = static_cast<uint8_t*>(modInfo.lpBaseOfDll);
+    const auto size = static_cast<uintptr_t>(modInfo.SizeOfImage);
+
+    uintptr_t pos = 0;
+    const uintptr_t searchLen = bytesStr.size();
+
+    for (auto* retAddress = start_offset; retAddress < start_offset + size; retAddress++) {
+        if (bytesStr[pos] == "??" || bytesStr[pos] == "?" || 
+            *retAddress == static_cast<uint8_t>(std::strtoul(bytesStr[pos].c_str(), nullptr, 16))) {
+            if (pos + 1 == bytesStr.size())
+                return (reinterpret_cast<uintptr_t>(retAddress) - searchLen + 1);
+            pos++;
+        }
+        else {
+            pos = 0;
+        }
+    }
+    return 0;
+}
 }
--- Gears/Memory/NativeMemory.hpp
+++ Gears/Memory/NativeMemory.hpp
@@ -5,6 +5,7 @@
 namespace mem {
 void init();
 uintptr_t FindPattern(const char* pattern, const char* mask); 
+uintptr_t FindPattern(const char* pattStr);
 std::vector<uintptr_t> FindPatterns(const char* pattern, const char* mask);
 extern uintptr_t(*GetAddressOfEntity)(int entity);
 extern uintptr_t(*GetModelInfo)(unsigned int modelHash, int* index);
