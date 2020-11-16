03b4dff823a95fa1eccb35941ba7f5c6d4c9d5e5
ikt <emily66666666ok@outlook.com>
2020-11-16 12:56:34 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-@call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
-@echo checking for nuget
-@if exist nuget.exe nuget.exe restore
-set ExternalCompilerOptions=
-msbuild /p:Platform=x64 /p:Configuration=Release /clp:Summary /nologo
