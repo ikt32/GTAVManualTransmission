2d7b11d80f594a96e445d335dac41d53f05f99b3
E66666666 <emily66666666ok@outlook.com>
2021-03-27 14:38:15 +0100

    Update version number

--- Gears/Constants.h
+++ Gears/Constants.h
@@ -3,8 +3,8 @@
 #define STR(x) STR_HELPER(x)
 
 #define VERSION_MAJOR 5
-#define VERSION_MINOR 1
-#define VERSION_PATCH 1
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
 
 namespace Constants {
     static const char* const DisplayVersion = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH);
