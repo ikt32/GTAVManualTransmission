ikt32/GTAVManualTransmission

commit c47eaca139e814e1b0c2616f7fea2acc36a2770c

Add check for game pause, cancel FFB

Gears/Memory/CTimer.h
@@ -1,0 +1,17 @@
+/*
+ * Zolika1351 - V SDK
+ */
+
+#pragma once
+
+#include "NativeMemory.hpp"
+#include "NewAddressSet.h"
+
+class CTimer
+{
+public:
+	static inline bool& m_CodePause = AddressSetter::GetRefFromRelative<bool>(mem::FindPattern("0A 05 ? ? ? ? 75 7A 48 8B 35") + 2);
+	static inline bool& m_UserPause = AddressSetter::GetRefFromRelative<bool>(mem::FindPattern("0A 05 ? ? ? ? 75 7A 48 8B 35") - 4);
+	static inline float& ms_fTimeStep = AddressSetter::GetTimeStep();
+	static inline uint32_t& m_FrameCounter = AddressSetter::GetFrameCounter();
+};

Gears/Memory/NewAddressSet.h
@@ -1,0 +1,117 @@
+/*
+ * Zolika1351 - V SDK
+ */
+
+#pragma once
+
+#pragma comment(lib, "version.lib")
+#include "NativeMemory.hpp"
+
+namespace AddressSetter
+{
+	auto timeRelatedAddr = mem::FindPattern("F3 0F ? ? ? ? ? ? 0F 94 C0 F6 81 4A 09 00 00 10");
+	auto clockRelatedAddr = mem::FindPattern("48 83 EC 28 89 0D ? ? ? ? 89 15");
+
+	float& GetTimeStep()
+	{
+		// 0x2D752E4
+		static auto p = timeRelatedAddr;
+
+		static auto origlocation = p; // F71F9E
+
+		uint64_t ptr = p + 4;
+
+		ptr = *(uint32_t*)ptr; // 1E0333E
+
+		ptr += 8;
+
+		return *(float*)(origlocation + ptr);
+	}
+
+	uint32_t& GetFrameCounter()
+	{
+		// 0x2D752E0
+		static auto p = timeRelatedAddr;
+
+		static auto origlocation = p; // F71F9E
+
+		uint64_t ptr = p + 4;
+
+		ptr = *(uint32_t*)ptr; // 1E0333E
+
+		ptr += 8;
+
+		return *(uint32_t*)(origlocation + ptr - 4);
+	}
+	//mov cs:dest, register
+	uint64_t ReadAddrFromMovFromReg(uint64_t location)
+	{
+		// value stored: 16D9776
+		// at: 637030
+		// points to: 1D107AC
+		// offset: 1D107A6
+		// +6
+
+		// value stored: 16D9774
+		// at: 637036
+		// points to: 1D107B0
+		// offset: 1D107AA
+		// +6
+
+		uint64_t origaddr = location;
+
+		int32_t offset32 = *(int32_t*)(origaddr + 2);
+
+		int64_t offset = offset32;
+
+		uint64_t ret = origaddr + offset + 6;
+
+		return ret;
+	}
+
+	uint64_t ReadAddrFromRelative(uint64_t location)
+	{
+		uint64_t origaddr = location;
+
+		auto offset32 = *(int32_t*)(origaddr);
+
+		int64_t offset = offset32;
+
+		uint64_t ret = origaddr + offset + 4;
+
+		return ret;
+	}
+
+	uint32_t& GetClockDays()
+	{
+		static auto p = clockRelatedAddr; // 63702C
+		return *(uint32_t*)(ReadAddrFromMovFromReg(p + 4));
+	}
+
+	uint32_t& GetClockMonths()
+	{
+		static auto p = clockRelatedAddr; // 63702C
+		return *(uint32_t*)(ReadAddrFromMovFromReg(p + 4));
+	}
+
+	template<typename T> T& GetRefFromMovFromReg(uint64_t addr, uint64_t offset = 0)
+	{
+		if (addr > 0xEFFFFFFFFFFFFFFF) return *reinterpret_cast<T*>(nullptr);
+		if (addr < 0x10000000) return *reinterpret_cast<T*>(nullptr);
+		return *reinterpret_cast<T*>(ReadAddrFromMovFromReg(addr) + offset);
+	}
+
+	template<typename T> T& GetRefFromRelative(uint64_t addr, uint64_t offset = 0)
+	{
+		if (addr > 0xEFFFFFFFFFFFFFFF) return *reinterpret_cast<T*>(nullptr);
+		if (addr < 0x10000000) return *reinterpret_cast<T*>(nullptr);
+		return *reinterpret_cast<T*>(ReadAddrFromRelative(addr) + offset);
+	}
+
+	template<typename T> T& GetRef(const char* pattern)
+	{
+		uint64_t addr = mem::FindPattern(pattern);
+
+		return *reinterpret_cast<T*>(addr);
+	}
+}

Gears/script.cpp
@@ -24,6 +24,7 @@
 #include "UDPTelemetry/Socket.h"
 #include "UDPTelemetry/UDPTelemetry.h"
 
+#include "Memory/CTimer.h"
 #include "Memory/MemoryPatcher.hpp"
 #include "Memory/Offsets.hpp"
 #include "Memory/VehicleFlags.h"
@@ -2955,6 +2956,30 @@ void ScriptTick() {
 
 bool initialized = false;
 
+void CheckPause() {
+    bool lastPauseState = false;
+    
+    while (true) {
+        bool pauseState = CTimer::m_UserPause || CTimer::m_CodePause;
+        if (lastPauseState != pauseState) {
+            logger.Write(DEBUG, "[Pause] State changed. Was %d, is %d", lastPauseState, pauseState);
+            logger.Write(DEBUG, "    User: %d, Code: %d", CTimer::m_UserPause, CTimer::m_CodePause);
+
+            if (pauseState) {
+                g_controls.PlayFFBCollision(0);
+                g_controls.PlayFFBDynamics(0, 0);
+            }
+            else {
+
+            }
+
+            lastPauseState = pauseState;
+        }
+
+        Sleep(50);
+    }
+}
+
 void ScriptMain() {
     if (!initialized) {
         logger.Write(INFO, "Script started");
@@ -2965,5 +2990,10 @@ void ScriptMain() {
         logger.Write(INFO, "Script restarted");
     }
     InitTextures();
+
+    std::thread([]() {
+        CheckPause();
+    }).detach();
+
     ScriptTick();
 }

Gears/Gears.vcxproj
@@ -274,8 +274,10 @@
     <ClInclude Include="Input\NativeInput.h" />
     <ClInclude Include="Input\USBNotify.h" />
     <ClInclude Include="LaunchControl.h" />
+    <ClInclude Include="Memory\CTimer.h" />
     <ClInclude Include="Memory\NativeMatrix.h" />
     <ClInclude Include="Memory\NativeVectors.h" />
+    <ClInclude Include="Memory\NewAddressSet.h" />
     <ClInclude Include="Memory\VehicleBone.h" />
     <ClInclude Include="ManualTransmission.h" />
     <ClInclude Include="Memory\Patcher.h" />

Gears/Gears.vcxproj.filters
@@ -566,6 +566,12 @@
       <Filter>Input</Filter>
     </ClInclude>
     <ClInclude Include="ScriptMenuUtils.h" />
+    <ClInclude Include="Memory\CTimer.h">
+      <Filter>Memory</Filter>
+    </ClInclude>
+    <ClInclude Include="Memory\NewAddressSet.h">
+      <Filter>Memory</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <Filter Include="include">

commit 7991f30a0577b00b5cbe772d411b09e78b3f13a4

Remove CTimer-related stuff
Fixes #132

Caused by c47eaca, more in the referred issue.

Gears/Memory/CTimer.h
@@ -1,17 +1,0 @@
-/*
- * Zolika1351 - V SDK
- */
-
-#pragma once
-
-#include "NativeMemory.hpp"
-#include "NewAddressSet.h"
-
-class CTimer
-{
-public:
-	static inline bool& m_CodePause = AddressSetter::GetRefFromRelative<bool>(mem::FindPattern("0A 05 ? ? ? ? 75 7A 48 8B 35") + 2);
-	static inline bool& m_UserPause = AddressSetter::GetRefFromRelative<bool>(mem::FindPattern("0A 05 ? ? ? ? 75 7A 48 8B 35") - 4);
-	static inline float& ms_fTimeStep = AddressSetter::GetTimeStep();
-	static inline uint32_t& m_FrameCounter = AddressSetter::GetFrameCounter();
-};

Gears/Memory/NewAddressSet.h
@@ -1,117 +1,0 @@
-/*
- * Zolika1351 - V SDK
- */
-
-#pragma once
-
-#pragma comment(lib, "version.lib")
-#include "NativeMemory.hpp"
-
-namespace AddressSetter
-{
-	auto timeRelatedAddr = mem::FindPattern("F3 0F ? ? ? ? ? ? 0F 94 C0 F6 81 4A 09 00 00 10");
-	auto clockRelatedAddr = mem::FindPattern("48 83 EC 28 89 0D ? ? ? ? 89 15");
-
-	float& GetTimeStep()
-	{
-		// 0x2D752E4
-		static auto p = timeRelatedAddr;
-
-		static auto origlocation = p; // F71F9E
-
-		uint64_t ptr = p + 4;
-
-		ptr = *(uint32_t*)ptr; // 1E0333E
-
-		ptr += 8;
-
-		return *(float*)(origlocation + ptr);
-	}
-
-	uint32_t& GetFrameCounter()
-	{
-		// 0x2D752E0
-		static auto p = timeRelatedAddr;
-
-		static auto origlocation = p; // F71F9E
-
-		uint64_t ptr = p + 4;
-
-		ptr = *(uint32_t*)ptr; // 1E0333E
-
-		ptr += 8;
-
-		return *(uint32_t*)(origlocation + ptr - 4);
-	}
-	//mov cs:dest, register
-	uint64_t ReadAddrFromMovFromReg(uint64_t location)
-	{
-		// value stored: 16D9776
-		// at: 637030
-		// points to: 1D107AC
-		// offset: 1D107A6
-		// +6
-
-		// value stored: 16D9774
-		// at: 637036
-		// points to: 1D107B0
-		// offset: 1D107AA
-		// +6
-
-		uint64_t origaddr = location;
-
-		int32_t offset32 = *(int32_t*)(origaddr + 2);
-
-		int64_t offset = offset32;
-
-		uint64_t ret = origaddr + offset + 6;
-
-		return ret;
-	}
-
-	uint64_t ReadAddrFromRelative(uint64_t location)
-	{
-		uint64_t origaddr = location;
-
-		auto offset32 = *(int32_t*)(origaddr);
-
-		int64_t offset = offset32;
-
-		uint64_t ret = origaddr + offset + 4;
-
-		return ret;
-	}
-
-	uint32_t& GetClockDays()
-	{
-		static auto p = clockRelatedAddr; // 63702C
-		return *(uint32_t*)(ReadAddrFromMovFromReg(p + 4));
-	}
-
-	uint32_t& GetClockMonths()
-	{
-		static auto p = clockRelatedAddr; // 63702C
-		return *(uint32_t*)(ReadAddrFromMovFromReg(p + 4));
-	}
-
-	template<typename T> T& GetRefFromMovFromReg(uint64_t addr, uint64_t offset = 0)
-	{
-		if (addr > 0xEFFFFFFFFFFFFFFF) return *reinterpret_cast<T*>(nullptr);
-		if (addr < 0x10000000) return *reinterpret_cast<T*>(nullptr);
-		return *reinterpret_cast<T*>(ReadAddrFromMovFromReg(addr) + offset);
-	}
-
-	template<typename T> T& GetRefFromRelative(uint64_t addr, uint64_t offset = 0)
-	{
-		if (addr > 0xEFFFFFFFFFFFFFFF) return *reinterpret_cast<T*>(nullptr);
-		if (addr < 0x10000000) return *reinterpret_cast<T*>(nullptr);
-		return *reinterpret_cast<T*>(ReadAddrFromRelative(addr) + offset);
-	}
-
-	template<typename T> T& GetRef(const char* pattern)
-	{
-		uint64_t addr = mem::FindPattern(pattern);
-
-		return *reinterpret_cast<T*>(addr);
-	}
-}

Gears/script.cpp
@@ -24,7 +24,6 @@
 #include "UDPTelemetry/Socket.h"
 #include "UDPTelemetry/UDPTelemetry.h"
 
-#include "Memory/CTimer.h"
 #include "Memory/MemoryPatcher.hpp"
 #include "Memory/Offsets.hpp"
 #include "Memory/VehicleFlags.h"